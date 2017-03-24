4ab7cdd47b7d96b31634e5c7667ec6ae6fd46add
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-03-24 22:20:27 +0100

    Make transitions extensible

--- js/elements/app-view.js
+++ js/elements/app-view.js
@@ -1,8 +1,8 @@
-import { finished } from '../animation'
+import { landingToRegister } from '../transitions/landing-to-register'
 
-const delay = (milliseconds) => new Promise((resolve) => {
-  setTimeout(resolve, milliseconds)
-})
+const transitions = {
+  'landing:register': landingToRegister
+}
 
 export class AppView extends HTMLElement {
   constructor (templateName, html) {
@@ -43,104 +43,13 @@ export class AppView extends HTMLElement {
    * @param {AppView} view
    */
   async transitionInto (view) {
+    const transition = transitions[ `${this.templateName}:${view.templateName}` ]
 
-    if (view.templateName !== 'register') {
-      this.hide()
-      view.show()
-      return
+    if (transition) {
+      return transition(this, view)
     }
 
-    const $title = this.querySelector('.landing-page > .title')
-    const $button = this.querySelector('.landing-page > .button')
-
-    const $header = view.querySelector('.page-header')
-    const $content = view.querySelector('.page-content')
-
-    $title.style.zIndex = 1
-    $header.style.backgroundColor = 'transparent'
-    $header.style.color = 'transparent'
-    $content.style.opacity = 0
-
-    const titleBox = $title.getBoundingClientRect()
-    const transform = titleBox.top + window.scrollY
-
-    const buttonAnimation = $button.animate(
-      [
-        { opacity: 1 },
-        { opacity: 0 }
-      ],
-      {
-        duration: 100,
-        fill: 'forwards',
-        easing: 'ease'
-      }
-    )
-
-    await finished(buttonAnimation)
-
-    const titleAnimation = $title.animate(
-      [
-        { transform: 'none' },
-        { transform: `translateY(-${transform}px) scale(.4) translateY(-${transform * 0.4 / 2}px) translateY(4px)` }
-      ],
-      {
-        duration: 300,
-        fill: 'forwards',
-        easing: 'ease-in-out'
-      }
-    )
-
-    view.show()
-
-    const contentAnimation = $content.animate(
-      [
-        { opacity: 0 },
-        { opacity: 1 }
-      ],
-      {
-        duration: 400,
-        delay: 150,
-        fill: 'forwards',
-        easing: 'ease'
-      }
-    )
-
-    await finished(titleAnimation)
-
-    const headerTextAnimation = $title.animate(
-      [
-        { color: 'currentColor' },
-        { color: '#fff' }
-      ],
-      {
-        duration: 200,
-        fill: 'forwards',
-        ease: 'ease-in'
-      }
-    )
-
-    const headerBackgroundAnimation = $header.animate(
-      [
-        { backgroundColor: 'transparent' },
-        { backgroundColor: 'var(--brand-color)' }
-      ],
-      {
-        duration: 200,
-        fill: 'forwards',
-        ease: 'ease-in'
-      }
-    )
-
-    await Promise.all([
-      finished(headerTextAnimation),
-      finished(headerBackgroundAnimation),
-      finished(contentAnimation)
-    ])
-
     this.hide()
-
-    $header.style.backgroundColor = ''
-    $header.style.color = ''
-    $content.style.opacity = ''
+    view.show()
   }
 }
