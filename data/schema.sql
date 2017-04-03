ca8f61001cd144d4becbae2caaa8ec50ac2f7f8c
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-04-03 20:23:02 +0200

    Clean up schema

--- data/schema.sql
+++ data/schema.sql
@@ -48,7 +48,6 @@ CREATE TABLE mill.question (
 
 CREATE INDEX question_category_id ON mill.question (category_id);
 
--- Todo: write trigger to ensure there's only one correct answer per question
 CREATE TABLE mill.answer (
   id BIGINT PRIMARY KEY DEFAULT mill.next_id(),
   title VARCHAR(255) NOT NULL,
@@ -56,17 +55,12 @@ CREATE TABLE mill.answer (
   question_id BIGINT NOT NULL REFERENCES mill.question (id) ON DELETE CASCADE
 );
 
--- Todo: do we need something else associated with the player?
--- Todo: do we need the name?
 CREATE TABLE mill.player (
   id BIGINT PRIMARY KEY DEFAULT mill.next_id(),
   name VARCHAR(255) NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT mill.utc_now()
 );
 
--- Todo: review naming of 'started_at' and 'ended_at'
--- Todo: review 'has_used_joker'
--- Todo: does setting 'started_at' make sence when creating the game or should that be done later on?
 CREATE TABLE mill.game (
   id BIGINT PRIMARY KEY DEFAULT mill.next_id(),
   has_used_joker BOOLEAN NOT NULL DEFAULT FALSE,
@@ -83,7 +77,6 @@ CREATE TABLE mill.game_category (
 
 CREATE UNIQUE INDEX game_category_unique ON mill.game_category (game_id, category_id);
 
--- Todo: use trigger to enforce one answer per (question, game)
 CREATE TABLE mill.game_answer (
   id BIGINT PRIMARY KEY DEFAULT mill.next_id(),
   game_id BIGINT NOT NULL REFERENCES mill.game (id) ON DELETE CASCADE,
@@ -93,7 +86,6 @@ CREATE TABLE mill.game_answer (
 
 CREATE UNIQUE INDEX game_answer_unique ON mill.game_answer (game_id, answer_id);
 
--- Todo: optimize this (remove dependency on view game_answered_question)
 CREATE OR REPLACE VIEW mill.game_question AS
   SELECT question.*, game.id AS game_id
   FROM mill.question AS question
@@ -105,58 +97,16 @@ CREATE OR REPLACE VIEW mill.game_question AS
       ON category.game_id = game.id
   GROUP BY question.id, game.id;
 
--- Todo: this table introduces redundant
+-- Todo: this table introduces redundant data
 -- Todo: ... but is required for the admin to be able to delete high score entries
 -- Todo: ... without deleting the user/game itself
--- Todo: review naming
 -- Todo: should we still reference the game?
 
 CREATE TABLE mill.score (
   id BIGINT PRIMARY KEY DEFAULT mill.next_id(),
   score INT NOT NULL,
+  weighted_score DOUBLE PRECISION NOT NULL,
   game_id BIGINT NULL REFERENCES mill.game (id) ON DELETE SET NULL
 );
 
 CREATE UNIQUE INDEX score_game_id ON mill.score (game_id);
-
-CREATE OR REPLACE FUNCTION mill.calculate_score(_game_id BIGINT) RETURNS INT AS $$
-  DECLARE
-    score INT;
-    has_lost BOOLEAN;
-  BEGIN
-    SELECT INTO score, has_lost
-      (count(1) FILTER (WHERE is_correct = TRUE)) * 30,
-      (count(1) FILTER (WHERE is_correct = FALSE)) > 0
-    FROM mill.game_answered_question AS answered
-    WHERE answered.game_id = _game_id;
-
-    IF has_lost THEN
-      RETURN 0;
-    END IF;
-
-    RETURN score;
-  END;
-$$ LANGUAGE PLPGSQL;
-
-CREATE OR REPLACE FUNCTION mill.update_score(_game_id BIGINT) RETURNS VOID AS $$
-DECLARE
-  _score INT;
-BEGIN
-  SELECT mill.calculate_score(_game_id) INTO _score;
-
-  INSERT INTO mill.score (score, game_id)
-  VALUES (_score, _game_id)
-  ON CONFLICT (game_id) DO UPDATE SET score = _score;
-END;
-$$ LANGUAGE PLPGSQL;
-
-CREATE OR REPLACE FUNCTION mill.update_score_on_insert() RETURNS TRIGGER AS $$
-BEGIN
-  PERFORM mill.update_score(NEW.game_id);
-  RETURN NEW;
-END;
-$$ LANGUAGE PLPGSQL;
-
-
-CREATE TRIGGER update_score AFTER INSERT ON mill.game_answer
-  FOR EACH ROW EXECUTE PROCEDURE mill.update_score_on_insert();
