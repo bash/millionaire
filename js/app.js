cc2f952336104ba0b5b0be13fc445819bc593bb3
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-04-08 14:35:25 +0200

    Remove <register-form>

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ import { AppRouter } from './elements/app-router'
 import { AppLink } from './elements/app-link'
 import { AppForm } from './elements/app-form'
 import { AppView } from './elements/app-view'
-import { RegisterForm } from './elements/register-form'
 import { EventButton } from './elements/event-button'
 import { AnswerButton } from './elements/answer-button'
 import { GameQuestion } from './elements/game-question'
@@ -14,7 +13,6 @@ window.customElements.define('app-router', AppRouter)
 window.customElements.define('app-link', AppLink, { extends: 'a' })
 window.customElements.define('app-form', AppForm, { extends: 'form' })
 window.customElements.define('app-view', AppView)
-window.customElements.define('register-form', RegisterForm)
 window.customElements.define('event-button', EventButton, { extends: 'button' })
 window.customElements.define('answer-button', AnswerButton, { extends: 'button' })
 window.customElements.define('game-question', GameQuestion)
